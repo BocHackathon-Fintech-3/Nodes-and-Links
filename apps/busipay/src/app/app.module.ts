abef0ee9510e30b2733b11efbf40702efa29afe6
nodesmichael <michael@nodeslinks.com>
2019-10-18 23:13:32 +0300

    Add angular material

--- apps/busipay/src/app/app.module.ts
+++ apps/busipay/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule, MatButtonModule} from '@angular/material';
+import { BocLoginComponent } from './components/boc-login/boc-login.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, BocLoginComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ apps/busipay/src/app/components/boc-login/boc-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'busipay-boc-login',
+  templateUrl: './boc-login.component.html',
+  styleUrls: ['./boc-login.component.scss']
+})
+export class BocLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
